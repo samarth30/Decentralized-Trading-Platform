9e30d3b58ba3ac14af0864f3d63327e70660ae2a
samarth30 <samarth.gugnani30@gmail.com>
2020-07-06 01:16:44 +0530

    loading error

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import Header from './Header.js';
 import Footer from './Footer.js';
+import Wallet from './Wallet.js';
 
 function App({web3, accounts, contracts}) {
   const [tokens, setTokens] = useState([]);
   const [user, setUser] = useState({
     accounts: [],
+    balances:{
+      tokenDex:0,
+      tokenWallet:0
+    },
     selectedToken: undefined
   });
 
@@ -20,11 +25,57 @@ function App({web3, accounts, contracts}) {
         ...token,
         ticker: web3.utils.hexToUtf8(token.ticker)
       }));
+      const balances = await getBalances(accounts[0],tokens[0]);
       setTokens(tokens);
-      setUser({accounts, selectedToken: tokens[0]});
+      setUser({accounts, balances,selectedToken: tokens[0]});
     }
     init();
   }, []);
+
+
+  const getBalances = async (account,token)=>{
+    const tokenDex = await contracts.dex.methods
+    .traderBalances(account,web3.utils.fromAscii(token.ticker))
+    .call();
+    const tokenWallet = await contracts[token.ticker].methods
+    .balanceOf(account)
+    .call(); 
+    return {tokenDex , tokenWallet};
+  }
+
+  const deposit = async amount =>{
+    await contracts[user.selectedToken.ticker].methods
+    .approve(contracts.dex.options.address , amount)
+    .send({from:user.accounts[0]});
+    await contracts.dex.methods
+    .deposit(
+      amount,
+      web3.utils.fromAscii(user.selectedToken.ticker)
+    )
+    .send({from:user.accounts[0]})
+    const balances = await getBalances(
+      user.accounts[0],
+      user.selectedToken
+    )
+    setUser(user => ({...user,balances}));
+  }
+
+  const withdraw = async amount =>{
+    await contracts.dex.methods
+    .withdraw(
+      amount,
+      web3.utils.fromAscii(user.selectedToken.ticker)
+    )
+    .send({from:user.accounts[0]})
+
+    const balances = await getBalances(
+      user.accounts[0],
+      user.selectedToken
+    )
+    setUser(user => ({...user,balances}));
+  }
+
+  
 
   if(typeof user.selectedToken === 'undefined') {
     return <div>Loading...</div>;
@@ -38,9 +89,17 @@ function App({web3, accounts, contracts}) {
         user={user}
         selectToken={selectToken}
       />
-      <div>
-        Main part
-      </div>
+      <main className="container-fluid">
+        <div className="row">
+          <div className="col-sm-4 first-col">
+            <Wallet
+            user={user}
+            deposit={deposit}
+            withdraw={withdraw}
+            />
+          </div>
+        </div>
+      </main>
       <Footer />
     </div>
   );
