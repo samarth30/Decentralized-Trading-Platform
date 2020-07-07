98190631e10754fd45fa78a2ef8b0e8e7690c478
samarth30 <samarth.gugnani30@gmail.com>
2020-07-07 18:04:58 +0530

    added all the components

--- truffle-config.js
+++ truffle-config.js
@@ -4,19 +4,6 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-  networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    development: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 7545, // Standard Ethereum port (default: none)
-      network_id: "*", // Any network (default: none)
-    },
-  },
   compilers: {
     solc: {
       version: '0.6.3'
